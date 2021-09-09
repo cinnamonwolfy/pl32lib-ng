f7f42d74cc09768c282c9fc941b0d9630410fccf
pocketlinux32 <pocketlinux32@gmail.com>
2021-09-09 16:59:24 -0500

    Combined pl32-string and pl32-math into pl32-parse. Still broken

--- src/pl32-string.c
+++ src/pl32-string.c
@@ -1,8 +1,28 @@
-// pl32-string.c: String manipulation module
+// pl32-parse.c: Parsing module
 #include <pl32-string.h>
 
+// A simple internal calculator
+double internal_calculate(double* numbers, char op){
+	switch(op){
+		case '+':
+			return numbers[0] + numbers[1];
+		case '-':
+			return numbers[0] - numbers[1];
+		case '*':
+			return numbers[0] * numbers[1];
+		case '/':
+			return numbers[0] / numbers[1];
+		case '^':
+			double tempNum = numbers[0];
+			for(int i = 0; i < numbers[1]; i++){
+				numbers[0] *= tempNum;
+			}
+			return numbers[0];
+	}
+}
+
 // Parses a string into an array
-parsedstr_t parse(char* input, char* delimiter){
+parsedstr_t strparse(char* input, char* delimiter){
 	if(delimiter == NULL){
 		delimiter = malloc(1);
 		*delimiter = ' ';
@@ -33,4 +53,52 @@ parsedstr_t parse(char* input, char* delimiter){
 	free(delimiter);
 
 	return returnStruct;
+}
+
+// internal_calculate wrapper with order of operations algorithm
+double calculate(calcstruct_t workStruct){
+	double result;
+
+	if((workStruct.size[1] + 1 != workStruct.size[0]) || (workStruct.size[0] <= 2 || workStruct.size[1] <= 1)){
+		return NULL;
+	}
+
+	for(int i = 0; i < workStruct.size[1]; i++){
+		if(ops[i] == '*' || ops[i] == '/'){
+			if(i != 0 && (ops[i - 1] == '*' || ops[i - 1] == '/')){
+				double tempNums[2] = {result, workStruct.numbers[i + 1]};
+				result = internal_calculate(tempNums, workStruct.ops[i]);
+				numbers[i + 1] = 0;
+			}else if(workStruct.size[1] == 1 || i == 0){
+				double tempNums[2] = {numbers[i], numbers[i + 1]};
+				result = internal_calculate(tempNums, workStruct.ops[i]);
+			}else{
+				double tempNums[2] = {workStruct.numbers[i], workStruct.numbers[i + 1]};
+				if(workStruct.ops[i - 1] == '-'){
+					result -= internal_calculate(tempNums, workStruct.ops[i]);
+				}else if(workStruct.ops[i - 1] == '+'){
+					result += internal_calculate(tempNums, workStruct.ops[i]);
+				}
+				numbers[i] = 0;
+				numbers[i + 1] = 0;
+			}
+		}
+	}
+
+	for(int i = 0; i < workStruct.size[1]; i++){
+		if(workStruct.ops[i] == '+' || workStruct.ops[i] == '-'){
+			if(result == 0 || i == 0){
+				double tempNums[2] = {workStruct.numbers[i], workStruct.numbers[i + 1]};
+				result += internal_calculate(tempNums, workStruct.ops[i]);
+			}else{
+				double tempNums[2] = {result, workStruct.numbers[i + 1]};
+				result = internal_calculate(tempNums, ops[i]);
+			}
+		}
+	}
+}
+
+// parses a string containing a mathematical expression
+double calculateString(char* workString){
+	parsedstr_t parsedWorkStr = string_parse(workString, " ");
 }
