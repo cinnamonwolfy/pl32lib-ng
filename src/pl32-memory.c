2552ce84ab17e8a36d0e509af476405351b8635f
pocketlinux32 <pocketlinux32@gmail.com>
2021-09-09 08:45:58 -0500

    Added comments to pl32-memory

--- src/pl32-memory.c
+++ src/pl32-memory.c
@@ -1,17 +1,19 @@
 //pl32-memory.c: Memory management and string parser module
 #include <pl32-memory.h>
 
+// Struct to track pointers and their sizes
 typedef struct pointerTracker{
 	void* pointer;
 	size_t size;
 } ptrtrack_t;
 
-ptrtrack_t* pointerStore = NULL;
-size_t ptrStoreSize = 0;
+ptrtrack_t* pointerStore = NULL; // Contains the tracker structs
+size_t ptrStoreSize = 0; // Size of pointerStore
 
-size_t allocMaxMemory = 128 * 1024 * 1024;
-size_t usedMemory = 0;
+size_t allocMaxMemory = 128 * 1024 * 1024; // Maximum amount of bytes it will allocate (Default = 128MB)
+size_t usedMemory = 0; // Amount of currently malloc'd memory in bytes
 
+// Finds a pointer within pointerStore. Returns -1 if it doesn't find the pointer
 int findPtr(void* ptr){
 	int i = 0;
 	bool found;
@@ -30,6 +32,7 @@ int findPtr(void* ptr){
 
 }
 
+// Adss a struct containing the pointer and the size of the pointer to pointerStore. Terminates the program on realloc() failure
 void addPtrEntry(void* ptr, size_t size){
 	if(pointerStore == NULL){
 		pointerStore = malloc(2 * sizeof(ptrtrack_t));
@@ -44,16 +47,18 @@ void addPtrEntry(void* ptr, size_t size){
 	ptrtrack_t* tempPtr = realloc(pointerStore, (ptrStoreSize + 1) * sizeof(ptrtrack_t));
 
 	if(!tempPtr){
-		exit(0);
+		exit(1);
 	}else{
 		pointerStore = tempPtr;
 		ptrStoreSize++;
 
 		pointerStore[ptrStoreSize - 1] = tempTracker;
+		usedMemory += size;
 	}
 
 }
 
+// Finds and removes a struct from pointerStore containing the pointer given. Terminates the program on realloc() failure
 void rmPtrEntry(void* ptr){
 	if(pointerStore == NULL){
 		pointerStore = malloc(2 * sizeof(ptrtrack_t));
@@ -68,19 +73,33 @@ void rmPtrEntry(void* ptr){
 	int i = findPtr(ptr);
 
 	if(i != -1){
-		pointerStore[i].pointer = NULL;
-		pointerStore[i].size = 0;
+		usedMemory -= pointerStore[i].size;
+		pointerStore[i].pointer = pointerStore[ptrStoreSize - 1].pointer;
+		pointerStore[i].size = pointerStore[ptrStoreSize - 1].size;
+
+		ptrtrack_t* tempPtr = realloc(pointerStore, (ptrStoreSize * sizeof(ptrtrack_t)) - sizeof(ptrtrack_t));
+
+		if(!tempPtr){
+			exit(1);
+		}else{
+			pointerStore = tempPtr;
+			ptrStoreSize--;
+
+		}
 	}
 }
 
+// Change memory allocation limit
 void changeAllocLimit(size_t bytes){
 	allocMaxMemory = bytes;
 }
 
+// Get current amount of used memory
 size_t getAllocSize(){
 	return usedMemory;
 }
 
+// Malloc wrapper that adds a struct to pointerStore
 void* safe_malloc(size_t size){
 	if((usedMemory + size) > allocMaxMemory){
 		return NULL;
@@ -92,6 +111,7 @@ void* safe_malloc(size_t size){
 	return retPtr;
 }
 
+// Calloc wrapper that adds a struct to pointerStore
 void* safe_calloc(size_t amount, size_t size){
 	if((usedMemory + size) > allocMaxMemory){
 		return NULL;
@@ -103,6 +123,7 @@ void* safe_calloc(size_t amount, size_t size){
 	return retPtr;
 }
 
+// Realloc wrapper that removes and add a struct to pointerStore
 void* safe_realloc(void* pointer, size_t size){
 	int pIndex = findPtr(pointer);
 	int newAllocSize = usedMemory;
@@ -126,11 +147,13 @@ void* safe_realloc(void* pointer, size_t size){
 	return retPtr;
 }
 
+// Free wrapper that removes a struct from pointerStore
 void safe_free(void* pointer){
 	rmPtrEntry(pointer);
 	free(pointer);
 }
 
+// Frees all pointers listed in pointerStore
 void safe_free_all(){
 	for(int i = 0; i < ptrStoreSize; i++){
 		if(pointerStore[i].pointer != NULL){
@@ -139,24 +162,32 @@ void safe_free_all(){
 			pointerStore[i].size = 0;
 		}
 	}
+
+	free(pointerStore);
+	pointerStore = malloc(2 * sizeof(ptrtrack_t);
 }
 
+// safe_malloc but casted to int pointer
 int* int_smalloc(size_t amount){
 	return (int*)safe_malloc(amount * sizeof(int));
 }
 
+// safe_realloc but casted to int pointer
 int* int_srealloc(int* pointer, size_t amount){
 	return (int*)safe_realloc(pointer, amount * sizeof(int));
 }
 
+// safe_malloc but casted to char pointer
 char* char_smalloc(size_t amount){
 	return (char*)safe_malloc(amount * sizeof(char));
 }
 
+// safe_realloc but casted to char pointer
 char* char_srealloc(char* pointer, size_t amount){
 	return (char*)safe_realloc(pointer, amount * sizeof(char));
 }
 
+// Parses a string into an array
 parsedstr_t parse(char* input, char* delimiter){
 	if(delimiter == NULL){
 		delimiter = malloc(1);
