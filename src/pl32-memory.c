162d8c9eae468d8dd27de5deb1a3436e74ede3b2
pocketlinux32 <pocketlinux32@gmail.com>
2022-01-23 14:20:14 -0600

    restructure

--- src/pl32-memory.c
+++ src/pl32-memory.c
@@ -23,13 +23,14 @@ struct plgc {
 	bool isInitialized;
 };
 
-// Central
+// Controller for garbage collector
 int plGCManage(plgc_t* gc, int mode, void* ptr, size_t size){
 	if(gc == NULL){
 		return 1;
 	}
 
 	switch(mode){
+		// Initializes the garbage collector
 		case PLGC_INIT:
 			if(!gc->isInitialized){
 				gc->upAmnt = 0;
@@ -40,18 +41,22 @@ int plGCManage(plgc_t* gc, int mode, void* ptr, size_t size){
 				gc->isInitialized = true;
 			}
 			break;
+		// Manually makes the garbage collector clean up memory
 		case PLGC_CLEAN:
-
 			break;
+		// Cleans memory for the last time and deallocates the garbage collector
 		case PLGC_STOP:
 			break;
+		// Adds pointer reference to the tracking array
 		case PLGC_ADDPTR:
 			break;
+		// Removes pointer reference from the tracking array
 		case PLGC_RMPTR:
 			break;
 	}
 }
 
+// malloc() wrapper that interfaces with the garbage collector
 void* plGCAlloc(plgc_t* gc, size_t size){
 	void* tempPtr;
 
@@ -62,6 +67,7 @@ void* plGCAlloc(plgc_t* gc, size_t size){
 	plGCManage(gc, PLGC_ADDPTR, tempPtr, size);
 }
 
+// calloc() wrapper that interfaces with the garbage collector
 void* plGCCalloc(plgc_t* gc, size_t amount, size_t size){
 	void* tempPtr;
 
@@ -72,6 +78,7 @@ void* plGCCalloc(plgc_t* gc, size_t amount, size_t size){
 	plGCManage(gc, PLGC_ADDPTR, tempPtr, size);
 }
 
+// realloc() wrapper that interfaces with the garbage collector
 void* plGCRealloc(plgc_t* gc, void* pointer, size_t size){
 	void* tempPtr = realloc(pointer, size);
 
@@ -81,4 +88,10 @@ void* plGCRealloc(plgc_t* gc, void* pointer, size_t size){
 
 	plGCManage(gc, PLGC_RMPTR, tempPtr, size);
 	plGCManage(gc, PLGC_ADDPTR, tempPtr, size);
+}
+
+// free() wrapper that interfaces with the garbage collector
+void plGCFree(plgc_t* gc, void* pointer){
+	free(pointer);
+	plGCManage(gc, PLGC_RMPTR, tempPtr, 0);
 }
