baa96afa36f3292559bbf6e3821fd91f518e45c6
pocketlinux32 <pocketlinux32@gmail.com>
2022-03-22 09:19:45 -0500

    nano

---
+++ tests/pl32-file-test.c
@@ -0,0 +1,14 @@
+#include <pl32-file.h>
+
+int main(int argc, const char* argv[]){
+	plgc_t* mainGC = plGCInit(8 * 1024 * 1024)
+
+	printf("Opening an existing file")
+	plfile_t* realFile = plFOpen("include/pl32-file.h", "r", gc);
+
+	if(!realFile){
+		printf("Error opening file. Exiting...");
+	}
+
+	return 0;
+}
