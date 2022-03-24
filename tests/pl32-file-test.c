dfc1dc6d423cdb648a336baef7f0070baae3a3c1
pocketlinux32 <pocketlinux32@gmail.com>
2022-03-23 21:56:58 -0500

    started working on the testcase for pl32-file

--- tests/pl32-file-test.c
+++ tests/pl32-file-test.c
@@ -1,12 +1,12 @@
 #include <pl32-file.h>
 
 int main(int argc, const char* argv[]){
-	plgc_t* mainGC = plGCInit(8 * 1024 * 1024)
-	char* stringBuffer = plGCAlloc(4096 * sizeof(char));
+	plgc_t* mainGC = plGCInit(8 * 1024 * 1024);
+	char stringBuffer[4096] = "";
 
 	printf("Opening an existing file...");
-	plfile_t* realFile = plFOpen("include/pl32-file.h", "r", gc);
-	plfile_t* memFile = plFOpen(NULL, "w+", gc);
+	plfile_t* realFile = plFOpen("include/pl32-file.h", "r", mainGC);
+	plfile_t* memFile = plFOpen(NULL, "w+", mainGC);
 
 	if(!realFile){
 		printf("Error opening file. Exiting...\n");
@@ -14,11 +14,13 @@ int main(int argc, const char* argv[]){
 
 	printf("Done\n");
 	plFGets(stringBuffer, 4095, realFile);
-	stringBuffer[4095] = '\0';
-	printf("Contents of include/pl32-file.h:\n%s", stringBuffer);
+	printf("Contents of include/pl32-file.h:\n%s\n", stringBuffer);
 
-	printf("Reading and writing to file-in-memory")
-	plFPuts()
+	printf("Reading and writing to file-in-memory");
+	plFPuts("test string getting sent to the yes", memFile);
+	plFGets(stringBuffer, 4095, memFile);
+	printf("Done\n");
+	printf("Contents of file-in-memory:\n%s\n", stringBuffer);
 
 	return 0;
 }
